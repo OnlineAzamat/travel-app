2ef1cf06e51206dc5a566965ec7acb00a49402e2
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-06-16 18:29:04 +0500

    Create: public elements/source and Button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Travel",
@@ -17,7 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        {children}
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
