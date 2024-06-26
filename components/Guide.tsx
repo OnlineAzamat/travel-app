29da4a1de39c6cd1c00120da493486ed827e835a
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-06-26 22:26:21 +0500

    Update: Guide section: add boat image

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -10,9 +10,21 @@ const Guide = () => {
           WE ARE HERE FOR YOU
         </p>
         <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
-          <h2>Guide You to Easy Path</h2>
-          <p>Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+          <h2 className="bold-20 lg:bold-62 xl:max-w-[390px]">Guide You to Easy Path</h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
         </div>
+      </div>
+
+      <div className="flexCenter max-container relative w-full border-2 border-blue-500">
+        <Image 
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+
+        <div></div>
       </div>
     </section>
   )
