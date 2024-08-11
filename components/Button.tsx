a82711e34837355815e0a6038480e7bcfd070a38
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-08-11 15:39:29 +0500

    Complete GetApp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,14 +7,14 @@ type ButtonProps = {
   variant: string;
 }
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-26 whitespace-nowrap">{title}</label>
+      <label className="bold-26 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
